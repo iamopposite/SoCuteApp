56d30105065cfe85a2548cf80fe48c1cb1371d80
Mikhail Fayto <opposite@sibears.org>
2023-06-19 01:51:40 +0200

    Initial Commit

---
+++ You are so cute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You are so cute
+//
+//  Created by Mikhail Fayto on 16.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
